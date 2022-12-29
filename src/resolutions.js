d34a40cde506df043aeb6a835001bfead967811d
ozrn <ozerenkevser92@gmail.com>
2022-12-29 16:02:06 +0300

    Add resolutions.js containing resolutions data

---
+++ src/resolutions.js
@@ -0,0 +1,52 @@
+const resolutions = [
+    {
+        key: 1,
+        title: "Bad habits",
+        randomRes: "Spend less time on social media"
+    },
+    {
+        key: 2,
+        title:"Bad habits",
+        randomRes: "Reduce stress on the job"
+    },
+    {
+        key: 3,
+        title: "Bad habits",
+        randomRes: "Stop procrastinating"
+    },
+    {
+        key: 4,
+        title: "Health/Body",
+        randomRes: "Drink more water",
+    },
+    {
+        key: 5, 
+        title: "Health/Body",
+        randomRes: "Wake up earlier"
+    },
+    {
+        key: 6,
+        title: "Health/Body",
+        randomRes: "Eat mindfully"
+    },
+    {
+        key: 7,
+        title: "Hobbies",
+        randomRes: "Learn how to code"
+    },
+    {
+        key: 8,
+        title: "Hobbies",
+        randomRes: "Read more"
+    },
+    {
+        key: 9,
+        title: "Attitude",
+        randomRes: "Try to be more positive"
+    },
+    {
+        key: 10,
+        title: "Attitude",
+        randomRes: "Enjoy life more"
+    }
+]
