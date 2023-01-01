735a30a34b4085c9d45c03b5196a508a54e4701a
ozrn <ozerenkevser92@gmail.com>
2023-01-01 13:32:01 +0300

    Add input data to an array once button is pressed

--- src/components/List.js
+++ src/components/List.js
@@ -3,23 +3,30 @@ import React from "react";
 export default function List() {
 
     const [resolutionText, setResolutionText] = React.useState("");
+    const [resolutions, setResolutions] = React.useState([]);
 
     function handleChange(e) {
         const newValue = e.target.value;
         setResolutionText(newValue);
     }
 
+    function addResolution() {
+        setResolutions(prevRes => {
+            return [...prevRes, resolutionText];
+        })
+    }
+
     return (
         <div className ="container">
           <div className ="form">
             <input onChange = {handleChange} type ="text" value = {resolutionText} />
-            <button>
+            <button onClick = {addResolution}>
               <span>Add</span>
             </button>
           </div>
           <div>
             <ul>
-              <li>New Year's Resolution</li>
+              {resolutions.map(resolution => <li>{resolution}</li>)}
             </ul>
           </div>
         </div>
